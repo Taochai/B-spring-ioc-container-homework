d7c125f863aba1605982b2fe9e5df2c11164ecce
tao.chai <tao.chai@thoughtworks.com>
2021-03-04 09:06:19 +0800

    [Tao Chai]: feat: answer for Q1 using ApplicationContext

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,22 +1,26 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class GreetingController implements ApplicationContextAware {
 
-    private final GreetingService greetingService;
+    private ApplicationContext applicationContext;
 
-    @Autowired
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
 
     @GetMapping("/greet")
     public String greet() {
-        return greetingService.sayHi();
+        return this.applicationContext.getBean(GreetingService.class).sayHi();
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 }
