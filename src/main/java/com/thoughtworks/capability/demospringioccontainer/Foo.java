468f40b3eff1970fb104cc47d0d78f6c46ec8eb3
tao.chai <tao.chai@thoughtworks.com>
2021-03-04 10:41:01 +0800

    [Tao Chai]: feat: answer3 for Q3 using @Autowired & @PostConstruct.

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,11 +1,20 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class Foo {
 
+    @Autowired
     private Bar bar;
+
+    @PostConstruct
+    public void init() {
+        bar.setFoo(this);
+    }
 
     public Foo(Bar bar) {
         this.bar = bar;
